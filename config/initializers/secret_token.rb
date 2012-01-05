98dc0616b5d5d7b67d30beddf34e4dd309087f15
Kai Chang <kai.h.chang@gmail.com>
2012-01-05 11:42:58 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tworobots::Application.config.secret_token = 'e67359614261e320965cd7439d4c4790b1369d22da5da6b35910cd3ad36b6c4f9e3ec896280f7b2716302702038173301a5dbdc855a883defcadcefa2a143f7d'
